3a8b63a1ab58598197df569322998a587f0f21b9
FME Böðvar Guðbjörn Jónsson <bodvar@fmedom.is>
2018-10-23 23:08:30 +0000

    Implemented a test for updating a cell and the API for it.

---
+++ src/server/api/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+  res.status(405).send({ error: "GET method not allowed, try OPTIONS." });
+});
+
+router.get("/update/:cell", (req, res) => {
+  //res.status(200).send({ game: updateCell(req.params.cell) });
+  res.send(JSON.stringify({
+    currentPlayer : "X",
+    grid :  [ ["X", "-", "-"],["-", "-", "-"],["-", "-", "-"] ],
+    moves : 7,
+    maxMoves : 9,
+    winner : "O",
+    validMove : false
+
+  }))
+});
+
+module.exports = router;
