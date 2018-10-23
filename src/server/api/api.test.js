90978b0739a2c8555994f6f56c20edb5c6be818e
FME Böðvar Guðbjörn Jónsson <bodvar@fmedom.is>
2018-10-23 23:14:22 +0000

    Refactored the code to be in the standard coding style

--- src/server/api/api.test.js
+++ src/server/api/api.test.js
@@ -1,23 +1,22 @@
-const request = require("supertest");
-const app = require("../../../app");
+const request = require('supertest')
+const app = require('../../../app')
 
-describe("GET /api/update/:cell", () => {
-  it("should return an object with an updated game (new move has been made)", async () => {
+describe('GET /api/update/:cell', () => {
+  it('should return an object with an updated game (new move has been made)', async () => {
     // Create two dimensional array to hold our game board
-    const cell = "0,0";
-    const column = expect.stringMatching(/[XO-]/);
-    const row = [column, column, column];
-    const expectedGrid = [row, row, row];
-    const res = await request(app).get(`/api/update/${cell}`);
-    const game = JSON.parse(res.text);
-    expect(res.status).toBe(200);
-    expect(game.maxMoves).toBe(9);
-    expect(game.moves).toBeGreaterThanOrEqual(0);
-    expect(game.moves).toBeLessThanOrEqual(9);
-    expect([true,false].includes(game.validMove)).toBe(true);
-    expect(["X", "O", "T", "-"].includes(game.winner)).toBe(true);
-    expect.arrayContaining(expectedGrid);
-    expect(["X", "O"].includes(game.currentPlayer)).toBe(true);
-    
-    })
-});
+    const cell = '0,0'
+    const column = expect.stringMatching(/[XO-]/)
+    const row = [column, column, column]
+    const expectedGrid = [row, row, row]
+    const res = await request(app).get(`/api/update/${cell}`)
+    const game = JSON.parse(res.text)
+    expect(res.status).toBe(200)
+    expect(game.maxMoves).toBe(9)
+    expect(game.moves).toBeGreaterThanOrEqual(0)
+    expect(game.moves).toBeLessThanOrEqual(9)
+    expect([true, false].includes(game.validMove)).toBe(true)
+    expect(['X', 'O', 'T', '-'].includes(game.winner)).toBe(true)
+    expect.arrayContaining(expectedGrid)
+    expect(['X', 'O'].includes(game.currentPlayer)).toBe(true)
+  })
+})
