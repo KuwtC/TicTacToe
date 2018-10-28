0f09e8ec8fe304aa8eb6681e20b213b97199afe8
FME Böðvar Guðbjörn Jónsson <bodvar@fmedom.is>
2018-10-28 19:08:12 +0000

    Added CORS so the site works on Heroku.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const api = require('./src/server/api/api')
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 
 app.use('/', express.static(path.join(__dirname, '/dist')))
 app.use('/api', api)
